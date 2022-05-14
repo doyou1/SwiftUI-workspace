5d6e9c1828dd46a355bfcec53726c57c637e7c99
doyou1 <tla149@daum.net>
2022-05-14 22:54:24 +0900

    31. A SwiftUI List, OutlineGroup and DisclosureGroup Tutorial

---
+++ OutlineGroupDemo/Shared/OutlineGroupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutlineGroupDemoApp.swift
+//  Shared
+//
+//  Created by AIS on 2022/05/14.
+//
+
+import SwiftUI
+
+@main
+struct OutlineGroupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

80ac4400220b5ea799da18a747406bb5b56f5757
doyou1 <tla149@daum.net>
2022-05-16 13:48:02 +0900

    35. Basic SwiftUI Graphics Drawing

---
+++ DrawDemo/Shared/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by AIS on 2022/05/16.
+//
+
+import SwiftUI
+
+struct MyShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addQuadCurve(
+            to: CGPoint(x: rect.minX, y: rect.maxY),
+            control: CGPoint(x: rect.midX, y:rect.midY)
+        )
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.closeSubpath()
+
+        return path
+    }
+}
+
+
+struct ContentView: View {
+    let colors = Gradient(colors:
+                                [Color.red,
+                                Color.yellow,
+                                Color.green, Color.blue,
+                                Color.purple]
+                            )
+
+    var body: some View {
+        MyShape()
+            .fill(RadialGradient(gradient: colors,
+                                center: .center,
+                                startRadius: CGFloat(0),
+                                endRadius: CGFloat(300)))
+            .background(LinearGradient(gradient: Gradient(colors: [Color.black, Color.white]),
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing))
+            .frame(width: 360, height: 350)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DrawDemo/Shared/DrawDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawDemoApp.swift
+//  Shared
+//
+//  Created by AIS on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct DrawDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

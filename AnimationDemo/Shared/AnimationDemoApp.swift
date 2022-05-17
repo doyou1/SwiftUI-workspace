f3eb1e302e7fada71d816ca15cb6d95fe1b5b15d
doyou1 <tla149@daum.net>
2022-05-17 09:21:29 +0900

    36. SwiftUI Animation and Transitions

---
+++ AnimationDemo/Shared/AnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationDemoApp.swift
+//  Shared
+//
+//  Created by AIS on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct AnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationDemo/Shared/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by AIS on 2022/05/16.
+//
+
+import SwiftUI
+
+extension AnyTransition {
+    static var fadeAndMove: AnyTransition {
+        AnyTransition.opacity.combined(with: .move(edge: .top))
+    }
+}
+
+struct ContentView: View {
+    @State private var isButtonVisible: Bool = true
+
+    var body: some View {
+        VStack {
+            Toggle(isOn: $isButtonVisible.animation(.linear(duration: 2))) {
+                Text("Show/Hide Button")
+            }.padding()
+
+            if isButtonVisible {
+                Button(action: {}) {
+                    Text("Example Button")
+                }
+                .font(.largeTitle)
+//                .transition(.fadeAndMove)
+                .transition(.asymmetric(insertion: .scale, removal: .slide))
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
